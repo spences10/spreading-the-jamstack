97e2e0a30addb9803f150320a34c48ab482fbd87
Scott Spence <spences10apps@gmail.com>
2019-01-23 16:19:45 +0000

    index for layout and footer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Theme from './theme'
 import BGImage from './bg-image'
+import Layout from './layout'
+import LayoutNoFooter from './layout-no-footer'
 
-export { BGImage }
+export { BGImage, Layout, LayoutNoFooter }
 export default Theme

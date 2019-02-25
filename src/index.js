267b7cfa0b66344568f7b2fec32c34939dcfb37e
spences10 <spences10apps@gmail.com>
2019-02-25 11:48:56 +0000

    export good things

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import Theme from './theme'
 import BGImage from './bg-image'
+import GoodThings from './goodThings'
 import Layout from './layout'
 import LayoutNoFooter from './layout-no-footer'
+import Theme from './theme'
 
-export { BGImage, Layout, LayoutNoFooter }
+export { BGImage, Layout, LayoutNoFooter, GoodThings }
 export default Theme

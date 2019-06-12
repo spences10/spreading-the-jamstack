3eee0042dab99c7994f90b69ec9f489d88c8ba4d
spences10 <spences10apps@gmail.com>
2019-06-12 12:32:45 +0100

    exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import BGImage from './BgImage'
-import FullScreenImage from './FullScreenImage'
-import Layout from './Layout'
-import LayoutNoFooter from './LayoutNoFooter'
+import { BGImage } from './BgImage'
+import { FullScreenImage } from './FullScreenImage'
+import { Layout } from './Layout'
+import { LayoutNoFooter } from './LayoutNoFooter'
 import Theme from './theme'
 
 export { BGImage, Layout, LayoutNoFooter, FullScreenImage }

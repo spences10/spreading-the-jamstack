e13d5cddf264379b06c190f01ef60089ff3c244c
spences10 <spences10apps@gmail.com>
2020-01-10 12:22:17 +0000

    add big image

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { BGImage } from './BgImage'
+import { BigImage } from './BigImage'
 import { FullScreenImage } from './FullScreenImage'
 import { Layout } from './Layout'
 import { LayoutNoFooter } from './LayoutNoFooter'
 import Theme from './theme'
 
-export { BGImage, Layout, LayoutNoFooter, FullScreenImage }
+export { BGImage, Layout, LayoutNoFooter, FullScreenImage, BigImage }
 export default Theme
